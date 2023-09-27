8299cd0d9e3a37c435fcf651e9d8cf5f60c6d50c
MalteHerrmann <malte@evmos.org>
2023-09-27 14:36:52 +0200

    adjust script for flow described in testing doc

--- replicate-v14-upgrade-problems/replicate-v14-upgrade-problems.sh
+++ replicate-v14-upgrade-problems/replicate-v14-upgrade-problems.sh
@@ -6,21 +6,11 @@ set -e
 DEV0=$(evmosd keys show -a dev0 --home $HOME/.tmp-evmosd)
 DEV1=$(evmosd keys show -a dev1 --home $HOME/.tmp-evmosd)
 DEV2=$(evmosd keys show -a dev2 --home $HOME/.tmp-evmosd)
+DEV3=$(evmosd keys show -a dev3 --home $HOME/.tmp-evmosd)
 VALADDR=$(evmosd q staking validators --node http://localhost:26657 | grep operator_address | cut -c 21-71)
 
 # Store the exemplary vesting schedule in a file
 echo '{"start_time":1689804000,"periods":[{"coins":"100000000000000000aevmos","length_seconds":272160000}]}' > vesting_schedule.json
-
-# Create another dev key and send funds to it
-evmosd keys add dev3 --home $HOME/.tmp-evmosd
-DEV3=$(evmosd keys show -a dev3 --home $HOME/.tmp-evmosd)
-evmosd tx bank send dev0 $DEV3 1000000000000000000aevmos \
---home $HOME/.tmp-evmosd \
---gas auto \
---gas-adjustment 2.0 \
---fees 100000000000000aevmos \
--b block \
--y
 
 # Create vesting accounts for three addresses:
 # - evmos1pxjncpsu2rd3hjxgswkqaenrpu3v5yxurzm7jp from dev0
