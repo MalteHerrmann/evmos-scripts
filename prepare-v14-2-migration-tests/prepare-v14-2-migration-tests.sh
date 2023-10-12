962a3595050bdcfc4d34bffe27f279012cda0a6e
MalteHerrmann <malte@evmos.org>
2023-10-12 13:53:56 +0200

    remove unnecessary var

--- prepare-v14-2-migration-tests/prepare-v14-2-migration-tests.sh
+++ prepare-v14-2-migration-tests/prepare-v14-2-migration-tests.sh
@@ -3,7 +3,6 @@
 # Exit on first error
 set -e
 
-DEV1=$(evmosd keys show -a dev1 --home $HOME/.tmp-evmosd)
 VALADDR=$(evmosd q staking validators --node http://localhost:26657 | grep operator_address | cut -c 21-71)
 
 # Set up a delegation for old strategic reserve (=dev1 in this example)
